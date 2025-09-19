b974ac6d00d530c73d9d45c7830d598b9e650eb4
d1y <chenhonzhou@gmail.com>
2025-09-19 17:31:51 +0800

    add demo

---
+++ README.md
@@ -0,0 +1,9 @@
+# @d1y/kitty
+
+小猫影视扩展源, @d1y 自用
+
+```bash
+bun parse.js result.json
+# http://192.168.1.x:8080/result.json
+npx http-server
+```
---
+++ x18/chaojifulisousuo.ts
@@ -0,0 +1,85 @@
+export default class chaojisousuo14 implements Handle {
+  getConfig() {
+    return <Iconfig>{
+      id: 'chaojifulisousuo',
+      name: '超级福利搜索',
+      type: 1,
+      api: 'https://chaojisousuo14.buzz',
+      nsfw: true,
+    }
+  }
+  async getCategory() {
+    return <ICategory[]>[
+      {
+        text: "亚洲性爱",
+        id: "33",
+      },
+      {
+        text: "热门事件",
+        id: "37",
+      },
+      {
+        text: "动漫肉番",
+        id: "34",
+      },
+    ]
+  }
+  async getHome() {
+    const url = `${env.baseUrl}/index.php/vod/type/id/${env.get('category')}/page/${env.get('page')}.html`
+    const html = await req(url)
+    const $ = kitty.load(html)
+    return $(".vodlist .listpic").toArray().map<IMovie>(el => {
+      const a = $(el).find("a")
+      const pic = $(el).find(".vodpic")
+      const name = $(el).find(".vodname")
+      const remark = $(el).find(".time")
+      return {
+        id: a.attr("href") ?? "",
+        cover: pic.attr("data-original") ?? "",
+        title: name.text() ?? "",
+        remark: remark.text(),
+        playlist: [],
+      }
+    })
+  }
+  async getDetail() {
+    const id = env.get("movieId")
+    const html = await req(`${env.baseUrl}${id}`)
+    const $ = kitty.load(html)
+    const img = $(".pull-left.pull-left-mobile1 img.lazy")
+    const playlist: IPlaylist[] = $("#playlist4 tr").toArray().map((item) => {
+      const a = $(item).find("a")
+      const id = a.attr("href") ?? ""
+      const text = a.text()
+      return { text, id }
+    })
+    return <IMovie[]>[{
+      title: img.attr("title") ?? "",
+      cover: img.attr("src") ?? "",
+      id: id,
+      playlist,
+    }]
+  }
+  async getSearch() {
+    const url = `${env.baseUrl}/index.php/vod/search/page/${env.get("page")}/wd/${env.get("keyword")}.html`
+    const html = await (await fetch(url)).text()
+    const $ = kitty.load(html)
+    return $(".show-list li").toArray().map<IMovie>(item => {
+      const _ = $(item).find("img") ?? ""
+      const a = $(item).find("a.play-img")
+      const remark = $($(item).find("dl.fn-left").toArray().at(-1)).find("dd").text()
+      const id = a.attr("href") ?? ""
+      const cover = _.attr("src") ?? ""
+      const title = _.attr("alt") ?? ""
+      return { id, cover, title, remark, playlist: [] }
+    })
+  }
+  async parseIframe() {
+    const url = `${env.baseUrl}${env.get('iframe')}`
+    const html = await req(url)
+    const $ = kitty.load(html)
+    const script = $("#bofang_box script").text()
+    const m3u8 = script.match(/"url":"(.*?)"/)![1].replace(/\\/g, '')
+    return m3u8
+  }
+}
