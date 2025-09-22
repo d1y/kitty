a06ea8995714c32bef018d3027956a971b67467a
d1y <chenhonzhou@gmail.com>
2025-09-23 01:23:18 +0800

    add jsdy

---
+++ ts/jsdy.ts
@@ -0,0 +1,92 @@
+// import { kitty, req, createTestEnv } from 'utils'
+
+export default class jsdy implements Handle {
+  getConfig() {
+    return <Iconfig>{
+      id: 'jsdy$',
+      name: "极速电影",
+      api: "https://32d.cc",
+      nsfw: false,
+      type: 1,
+    }
+  }
+  async getCategory() {
+    return [
+      { text: '电影', id: "1" },
+      { text: '电视剧', id: "2" },
+      { text: '综艺', id: "3" },
+      { text: '动漫', id: "4" },
+      { text: '短剧', id: "5" },
+    ]
+  }
+  async getHome() {
+    const cate = env.get('category')
+    const page = env.get('page')
+    const url = `${env.baseUrl}/index.php/vod/type/id/${cate}/page/${page}.html`
+    const html = await req(url)
+    const $ = kitty.load(html)
+    return $(".stui-vodlist__item").toArray().map<IMovie>(item => {
+      const a = $(item).find("a.stui-vodlist__thumb")
+      const id = a.attr("href") ?? ""
+      const title = a.attr("title") ?? ""
+      const cover = a.find("img").attr("data-original") ?? ""
+      return <IMovie>{ id, title, cover }
+    })
+  }
+  async getDetail() {
+    const id = env.get<string>("movieId")
+    const url = `${env.baseUrl}${id}`
+    const html = await req(url)
+    const $ = kitty.load(html)
+    const a = $(".stui-content.col-pd a.stui-vodlist__thumb")
+    const title = a.attr("title") ?? ""
+    const cover = a.find("img").attr("data-original") ?? ""
+    const desc = ($(".stui-content__desc").text() ?? "").trim()
+    const player = $(".stui-content__playlist li").toArray().map(item => {
+      const a = $(item).find("a")
+      const id = a.attr("href") ?? ""
+      const text = a.attr("title") ?? ""
+      return <IPlaylistVideo>{ id, text }
+    })
+    const playlist = <IPlaylist>{ title: "极速播放", videos: player }
+    return <IMovie>{ id, title, cover, desc, remark: "", playlist: [playlist] }
+  }
+  async getSearch() {
+    const page = env.get<string>("page")
+    const wd = env.get<string>("keyword")
+    const url = `${env.baseUrl}/index.php/vod/search/page/${page}/wd/${wd}.html`
+    const html = await req(url)
+    const $ = kitty.load(html)
+    return $(".stui-vodlist__item").toArray().map<IMovie>(item => {
+      const a = $(item).find("a.stui-vodlist__thumb")
+      const id = a.attr("href") ?? ""
+      const title = a.attr("title") ?? ""
+      const cover = a.find("img").attr("data-original") ?? ""
+      return <IMovie>{ id, title, cover }
+    })
+  }
+  async parseIframe() {
+    const iframe = env.get<string>("iframe")
+    const html = await req(`${env.baseUrl}${iframe}`)
+    const m3u8 = html.match(/"url"\s*:\s*"([^"]+\.m3u8)"/)![1]
+    const m3u8Str = m3u8.replaceAll("\\/", "/")
+    return m3u8Str
+  }
+}
+
+// TEST
+// const env = createTestEnv("https://32d.cc")
+// const tv = new jsdy();
+// (async () => {
+//   const cates = await tv.getCategory()
+//   env.set("category", cates[0].id)
+//   env.set("page", 1)
+//   const home = await tv.getHome()
+//   env.set('keyword', '我能')
+//   const search = await tv.getSearch()
+//   env.set("movieId", search[0].id)
+//   const detail = await tv.getDetail()
+//   env.set("iframe", detail.playlist![0].videos[0].id)
+//   const realM3u8 = await tv.parseIframe()
+//   debugger
+// })()
