e70f4c5177079bff66ef07a59d921bfa82b53558
d1y <chenhonzhou@gmail.com>
2025-09-24 20:17:06 +0800

    fix NODE_TLS env

--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'fs'
 import { load } from 'cheerio'
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 export async function req(
   urlOrOptions: string | KittyRequestOptions,
